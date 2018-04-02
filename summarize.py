deb8b1115ee94cd0c58e2a1e189f3fc7a202a791
Alan Mitchell <tabb99@gmail.com>
2018-04-01 20:43:51 -0800

    Finished graphing. Controlled new record collection.

--- summarize.py
+++ summarize.py
@@ -22,40 +22,42 @@ url = 'https://api.enphaseenergy.com/api/v2/systems/{}/'.format(settings.SYSTEM_
 # path to this directory
 APP_PATH = dirname(realpath(__file__))
 
-FN_LAST_TS = join(APP_PATH, 'last_ts')
-FN_RECORDS = join(APP_PATH, 'records.csv')
+if settings.COLLECT:
 
-payload_base = {
-    'key': settings.API_KEY,
-    'user_id': settings.USER_ID,
-}
+    FN_LAST_TS = join(APP_PATH, 'last_ts')
+    FN_RECORDS = join(APP_PATH, 'records.csv')
 
-try:
-    start_at = int(open(FN_LAST_TS).read())
-except:
-    start_at = requests.get(url + 'summary', params=payload_base).json()['operational_at']
+    payload_base = {
+        'key': settings.API_KEY,
+        'user_id': settings.USER_ID,
+    }
 
-payload = payload_base.copy()
+    try:
+        start_at = int(open(FN_LAST_TS).read())
+    except:
+        start_at = requests.get(url + 'summary', params=payload_base).json()['operational_at']
 
-for i in range(9):
-    time.sleep(2)
-    print(start_at)
-    payload['start_at'] =  start_at
-    res = requests.get(url + 'stats', params=payload).json()
-    if 'intervals' not in res:
-        break
-    recs = map(lambda r: (r['end_at'], r['devices_reporting'], r['powr']), res['intervals'])
-    if len(recs):
-        with open(FN_RECORDS, 'a') as fout:
-            for rec in recs:
-                fout.write('{},{},{}\n'.format(*rec))
-        start_at = recs[-1][0]
-    else:
-        # sometimes there will be a 24 hour period without any reports, so advance
-        # the starting time a day and try again.
-        start_at += 24*3600
+    payload = payload_base.copy()
 
-open(FN_LAST_TS, 'w').write(str(start_at))
+    for i in range(9):
+        time.sleep(2)
+        print(start_at)
+        payload['start_at'] =  start_at
+        res = requests.get(url + 'stats', params=payload).json()
+        if 'intervals' not in res:
+            break
+        recs = map(lambda r: (r['end_at'], r['devices_reporting'], r['powr']), res['intervals'])
+        if len(recs):
+            with open(FN_RECORDS, 'a') as fout:
+                for rec in recs:
+                    fout.write('{},{},{}\n'.format(*rec))
+            start_at = recs[-1][0]
+        else:
+            # sometimes there will be a 24 hour period without any reports, so advance
+            # the starting time a day and try again.
+            start_at += 24*3600
+
+    open(FN_LAST_TS, 'w').write(str(start_at))
 
 # Analyzes 5 minute Production Data from 11400 Mtn Lake System
 
@@ -98,11 +100,6 @@ savefig('output/last10.png')
 df[str(dfd.index[-1].date())].plot(legend=False)
 ylabel('Power Produced Today, Watts')
 xlabel('Time')
-df_sum = df.resample('1M').sum() / 12000.
-df_sum.columns = ['kWh']
-df_sum.plot(marker='o', linewidth=1, legend=False)
-ylabel('kWh in Month')
-xlabel('Month')
 savefig('output/last_day.png')
 
 dfm = df.resample('1M').sum() / 12000.
@@ -127,7 +124,6 @@ xlabel('Day of Year')
 ylim(0, 2500);
 savefig('output/cum_kwh.png')
 
-
 dfcs = dfdp.cumsum().dropna()
 lr = dfcs.iloc[-1]
 ahead = lr[2018] - lr[2017]
@@ -144,21 +140,24 @@ dfbp = dfb.pivot_table(values='power', index='Hour', columns='mo', aggfunc='mean
 dfbp.columns = MONTH_NAMES
 dfbp.plot(subplots=True, layout=(4, 3), figsize=(12, 16), sharex=True, sharey=True)
 yticks(range(0, 3000, 500));
+savefig('output/monthly_profile.png')
 
+clf()
 dfb.groupby('mo').agg('max')['power'].plot(marker='o', linewidth=1)
 ylabel('Maximum Power Production, Watts')
 xticks(range(0,13))
 gca().set_xticklabels([''] + MONTH_NAMES);
+savefig('output/max_power.png')
 
 dfd = df.resample('1D').sum() / 12000.
 dfd.columns = ['Daily kWh']
 dfd['mo'] = dfd.index.month
 dfd.boxplot(by='mo')
-
 gca().set_xticklabels(MONTH_NAMES)
 xlabel('')
 ylabel('kWh in Day')
 title('')
+savefig('output/monthly_box.png')
 
 dfd = df.resample('1D').sum() / 12000.
 dfd.columns = ['kWh']
@@ -166,8 +165,9 @@ dfd['day_of_year'] = dfd.index.dayofyear
 dfd.plot.scatter(x='day_of_year', y='kWh', s=4)
 ylabel('kWh in Day')
 xlabel('Day of Year');
+savefig('output/daily_production.png')
 
 # Highest Energy Day
-print(dfd.kWh.idxmax())
-df['2017-06-15'].plot();
+# print(dfd.kWh.idxmax())
+# df['2017-06-15'].plot();
 
