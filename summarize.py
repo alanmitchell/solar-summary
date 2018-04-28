7825466fb1af26ae5fed552afd38cb9e7600cd52
Alan Mitchell <tabb99@gmail.com>
2018-04-28 11:05:42 -0800

    Needed to drop dupes for 5 minute fill.

--- summarize.py
+++ summarize.py
@@ -109,6 +109,7 @@ def get_data(use_dst=True):
         akst_adj = timedelta(hours=9)
         dfd['dts'] = dfd.dts - akst_adj
     dfd.set_index('dts', inplace=True)
+    dfd.drop_duplicates(keep='last', inplace=True)
     dfd = dfd.asfreq('5T', fill_value=0.0)
     if use_dst:
         dfd.index = dfd.index.tz_localize('UTC').tz_convert('US/Alaska').tz_localize(None)
