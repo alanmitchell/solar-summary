60647d50645ec1464e91b8b7d974be86c26c70de
Alan Mitchell <tabb99@gmail.com>
2021-09-03 12:08:36 -0800

    Better docs, and change in API call stop trigger.

--- summarize.py
+++ summarize.py
@@ -11,15 +11,11 @@ file in the script directory to run, and that file should be patterned after
 
 For continual use, you can run the Cron job every 10 minutes and stay within
 the Enphase free tier API limits (10,000 API calls per month), becuase each run
-of the script will generally call the API twice.  If you are loading historical
+of the script will generally call the API once.  If you are loading historical
 data into the system, you can probably run the script every minute if you are
 loading a couple years of historical data; the script won't exceed the per minute
 API limit (10 calls / minute), and you will be done loading data in (total days) / 9
-calls to the script, since one call to the script will load 9 days of data.
-
-(Note that with some additional code, we could reduce the number of API calls per
-script run to 1 instead of 2.  Test for a small number of records returned, like
-3 or less, and then don't call again if so.)
+minutes, since one call to the script will load 9 days of data.
 
 There are some plotting commands (e.g. tick spacing, axis limits) that improve 
 formatting for my particular solar system; you will need to change those 
@@ -68,25 +64,33 @@ if settings.COLLECT:
 
     for i in range(9):
         time.sleep(2)
+
         print(start_at)
         payload['start_at'] =  start_at
         res = requests.get(url + 'stats', params=payload).json()
+
         if 'intervals' not in res:
             break
+
         recs = list(map(lambda r: (r['end_at'], r['devices_reporting'], r['powr']), res['intervals']))
         if len(recs):
             with open(FN_RECORDS, 'a') as fout:
                 for rec in recs:
                     fout.write('{},{},{}\n'.format(*rec))
             start_at = recs[-1][0]
+
         else:
             # sometimes there will be a 24 hour period without any reports, so advance
             # the starting time a day and try again.  Only do this if it will not advance
             # beyond the current time.
             if time.time() > start_at + 24*3600:
                 start_at += 24*3600
-        # if we are within a week of the current time, dont't call the API again.
-        if start_at > time.time() - 3600*24*7:
+
+        # if we are within two weeks of the current time, don't call the API again.
+        # If we're within 2 weeks, this is probably normal use of the script as opposed
+        # to loading historical data.  If it is the last days of loading historical data,
+        # it will only take 14 more runs of the script to finish up, which isn't that long.
+        if start_at > time.time() - 3600 * 24 * 14:
             break
 
     open(FN_LAST_TS, 'w').write(str(start_at))
